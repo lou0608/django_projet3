c01cc13748e5077e477ca0caf9519b54d61547ec
ldoussiet <loudoussiet@gmail.com>
2022-06-02 16:24:41 +0200

    conserver juste données pred about

--- prediction/co_app/form2.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from prediction.models import ChasseursInscription 
-
-
-class ChasseursForm(forms.ModelForm):
-    class Meta:
-        model = ChasseursInscription
-        fields = "__all__"
